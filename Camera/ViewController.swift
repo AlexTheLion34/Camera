69b84d9e672c764ad2c9abfa78b59c2d4b50c697
AlexTheLion <alx_pet@mail.ru>
2019-02-06 15:15:22 +0300

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Alexey Petrenko on 06.02.2019.
+//  Copyright © 2019 Alexey Petrenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

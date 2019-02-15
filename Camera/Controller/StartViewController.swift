fbe2f0ffcd9b9c568a1b72e6400dbec0698033cb
AlexTheLion <alx_pet@mail.ru>
2019-02-15 22:34:17 +0300

    Refactor; improvements

---
+++ Camera/Controller/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  Camera
+//
+//  Created by Alexey Petrenko on 14.02.2019.
+//  Copyright © 2019 Alexey Petrenko. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

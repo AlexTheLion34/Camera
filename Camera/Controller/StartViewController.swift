5ccae5931a149fc8218ceadca3ba87f731475fd9
AlexTheLion <alx_pet@mail.ru>
2019-02-21 01:01:29 +0300

    Added keybord

--- Camera/Controller/StartViewController.swift
+++ Camera/Controller/StartViewController.swift
@@ -10,18 +10,78 @@ import UIKit
 
 class StartViewController: UIViewController {
     
-    private func setupNavigationBar() {
-        navigationController?.navigationBar.shadowImage = UIImage()
-        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 21 / 255, green: 101 / 255, blue: 192 / 255, alpha: 1)
-        navigationController?.navigationBar.tintColor = .white
-        navigationController?.navigationBar.topItem?.title = ""
-    }
-}
-
-extension StartViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var ipTextField: UITextField!
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        setupTextField()
+        registerForKeyboardNotifications()
+    }
+    
+    deinit {
+        removeKeyboardNotifications()
+    }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        
     }
 }
+
+// MARK: - NavigationBar settings
+extension StartViewController {
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 21 / 255, green: 101 / 255, blue: 192 / 255, alpha: 1)
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.topItem?.title = "Setup"
+    }
+}
+
+// MARK: - Keyboard settings
+extension StartViewController {
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let keyboardFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        scrollView.contentOffset = CGPoint(x: 0, y: keyboardFrameSize.height / 2)
+    }
+
+    @objc private func keyboardWillHide() {
+        scrollView.contentOffset = CGPoint.zero
+    }
+}
+
+// MARK: - TextFiled settings
+extension StartViewController: UITextFieldDelegate {
+    
+    private func setupTextField() {
+        ipTextField.delegate = self
+        ipTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingDidEnd)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        startButton.isEnabled = true
+    }
+}
+
+
